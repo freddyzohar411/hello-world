3d64f5688a32b393299103e249428bf213f5fa18
freddyzohar411 <91535657+freddyzohar411@users.noreply.github.com>
2021-11-29 04:50:49 +0800

    Update request-test.js

--- request-test.js
+++ request-test.js
@@ -1,5 +1,9 @@
 app.get('/search', (req, res) => {
-const results = db.search(req.query.product);
+  
+const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
+  
+const results = db.search(re.test(req.query.product));
 if (results.length === 0) {
   return res.send('<p>No results found for "' + req.query.product + '"</p>');
 }
