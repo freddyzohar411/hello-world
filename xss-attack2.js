99da6bc63f4a143b5352a77aef4a9323c28fad5a
freddyzohar411 <91535657+freddyzohar411@users.noreply.github.com>
2021-11-28 23:31:18 +0800

    Update xss-attack2.js

--- xss-attack2.js
+++ xss-attack2.js
@@ -1,7 +1,12 @@
-app.get('/search', (req, res) => {
-const results = db.search(req.query.product);
-if (results.length === 0) {
-  return res.send('<p>No results found for "' + req.query.product + '"</p>');
-}
+var xssFilters = require('xss-filters');
 
+app.get('/search', (req, res) => {
+  
+var productParam = req.query.product;  
+const results = db.search(req.query.product);
+
+if (results.length === 0) {
+res.send('<p>No result found for "' + xssFilters.inHTMLData(productParam) + '"</p>');
+}  
+  
 });
