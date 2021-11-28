241531ca6b67dfccf28771edea25944bda151fa8
freddyzohar411 <91535657+freddyzohar411@users.noreply.github.com>
2021-11-29 04:08:29 +0800

    new-sql-injection

---
+++ new-sql-injection.js
@@ -0,0 +1,8 @@
+app.post("/records", (request, response) => {
+const data = request.body;
+const query = `SELECT * FROM health_records WHERE id = (${data.id})`;
+connection.query(query, (err, rows) => {
+if(err) throw err;
+response.json({data:rows});
+});
+});
