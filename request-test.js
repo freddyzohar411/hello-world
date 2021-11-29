1dccd2c476e955d233822f15af81f753e45d95d3
freddyzohar411 <91535657+freddyzohar411@users.noreply.github.com>
2021-11-29 10:19:54 +0800

    Update request-test.js

--- request-test.js
+++ request-test.js
@@ -3,7 +3,7 @@ app.get('/search', (req, res) => {
 var re_new = String(req).replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
 const results = db.search(re_new.query.product));
 if (results.length === 0) {
-  return res.send('<p>No results found for "' + req.query.product + '"</p>');
+  return res.send('<p>No results found for "' + re_new.query.product + '"</p>');
 }
 
 });
